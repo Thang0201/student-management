63e6af9b95198877a32a4884a16917af167de909
thang0201 <95296977+thangptit01@users.noreply.github.com>
2023-09-19 10:43:22 +0700

    Add feature/update_and_pagination.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,9 @@ const phoneInput = $("#phone");
 const maleCheckbox = $("#male");
 const femaleCheckbox = $("#female");
 
-let studentList = $("#student-list");
+// const paginationList = $("pagination-list");
 
-// const LIMIT = 25;
-// const TOTAL_PAGE = students.length() / LIMIT;
-// let startIndex = 0;
+let studentList = $("#student-list");
 
 function Student(name, email, phone, address, gender) {
   this.name = name;
@@ -41,7 +39,7 @@ function renderStudent(students) {
             <p style="cursor: pointer; color: red" onclick="handleDelete(this,${index})">
               Xóa
             </p>
-            <p style="cursor: pointer; color: #0066FF" onclick="handleFix()">Sửa</p>
+            <p style="cursor: pointer; color: #0066FF" onclick="handleFix(${index})">Sửa</p>
           </td>
               </tr>
             `;
@@ -89,7 +87,7 @@ function checkEmail(input) {
   }
 }
 
-function validatePhone(input) {
+function checkPhone(input) {
   const rePhone = /^[0-9]+$/; // Số điện thoại chỉ chấp nhận ký tự số
 
   if (rePhone.test(input.value.trim())) {
@@ -98,7 +96,7 @@ function validatePhone(input) {
     showError(input, "Số điện thoại không hợp lệ. Chỉ nhập số.");
   }
 }
-
+// giới tính
 function getSelectedGender() {
   if (maleCheckbox.checked) {
     return maleCheckbox.value;
@@ -107,12 +105,44 @@ function getSelectedGender() {
   }
 }
 
+// kiểm tra xem email có trùng lặp không
+function isEmailRepeat(email) {
+  let isRepeat = false;
+  students.forEach((student) => {
+    if (emailInput.value === student.email) {
+      showError(email, "Email đã có trong danh sách. Vui lòng nhập lại");
+      isRepeat = true;
+    } else showSuccess(email);
+  });
+  return isRepeat;
+}
+
+// xóa 1 sinh viên
 function handleDelete(element, index) {
-  if (confirm(`Bạn có chắc chắn muốn xóa sinh viên này không?`)) {
+  if (
+    confirm(
+      `Bạn có chắc chắn muốn xóa sinh viên ${students[index].name} không?`
+    )
+  ) {
     students.splice(index, 1);
     localStorage.setItem("students", JSON.stringify(students));
     renderStudent(students);
   }
+}
+
+// thực hiện hiển thị sinh viên từ bảng vào form
+function editForm(index) {
+  const student = students[index];
+  nameInput.value = student.name;
+  emailInput.value = student.email;
+  phoneInput.value = student.phone;
+  addressInput.value = student.address;
+}
+
+// sửa thông tin sinh viên
+function handleFix(index) {
+  editForm(index);
+  renderStudent(students[index]);
 }
 
 form.addEventListener("submit", function (e) {
@@ -120,9 +150,10 @@ form.addEventListener("submit", function (e) {
 
   if (
     !existEmpty([nameInput, emailInput, phoneInput, addressInput]) &&
-    (maleCheckbox.checked || femaleCheckbox.checked)
+    (maleCheckbox.checked || femaleCheckbox.checked) &&
+    !isEmailRepeat(emailInput)
   ) {
-    validatePhone(phoneInput);
+    checkPhone(phoneInput);
     checkEmail(emailInput);
 
     if (!document.querySelector(".error")) {
@@ -149,3 +180,27 @@ form.addEventListener("submit", function (e) {
     );
   }
 });
+
+// const LIMIT = 4;
+// const TOTAL_PAGE = Math.ceil(students.length() / LIMIT);
+// let startIndex = 0;
+
+// function renderPagination(total_pages) {
+//   let paginationHTML = `<li onclick="handlePageChange(1)">Trước</li>`;
+//   for (let i = 1; i <= total_pages; i++) {
+//     paginationHTML += `<li onclick="handlePageChange(${i})">${i}</li>`;
+//   }
+//   paginationHTML += `<li onclick="handlePageChange(${total_pages})">Sau</li>`;
+//   paginationList.innerHTML = paginationHTML;
+// }
+
+// function handleChangePage(currentPage) {
+//   startIndex = (currentPage - 1) * LIMIT;
+//   newLimit = startIndex + LIMIT;
+
+//   let newListStudent = [];
+//   for (startIndex; startIndex <= newLimit; startIndex++) {
+//     newListStudent.push(students[startIndex]);
+//   }
+//   renderStudent(newListStudent);
+// }
